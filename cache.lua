3a57137761f2aa908c17456997f8dbdd0bf628c9
zhaozg <zhaozg@gmail.com>
2018-07-27 09:31:23 +0800

    add cache

---
+++ cache.lua
@@ -0,0 +1,148 @@
+-- cache.lua
+-- Simple Cache Server with TTL
+--[[lit-meta
+name = "zhaozg/luvit-cache"
+version = "0.0.1"
+description = "Simple Cache Server with TTL"
+tags = { "lua", "lit", "luvit", "cache"}
+license = "Apache 2.0"
+author = { name = "zhaozg", email = "zhaozg@gmail.com" }
+homepage = "https://github.com/zhaozg/luvit-cache"
+]]
+
+local Emitter = require('core').Emitter
+local timer = require'timer'
+local uv = require'uv'
+
+local type, table = type, table
+-------------------------------------------------------------------------------
+-- 缓存对象存储器
+-- 如果 ttl 为number类型, 整数部分表示秒，小数部分表示微秒, 默认为 300
+-- 如果 ttl 为boolean类型，true表示永久存储，需手动清除, false 表示仅读取一次生效
+
+local function s2ms(s)
+  return 1000*s
+end
+
+local function us2ms(us)
+  return us/1000
+end
+
+local function msnow()
+  uv.update_time()
+  return uv.now()
+end
+
+local Cache = Emitter:extend()
+function Cache:initialize(ttl, interval)
+  local vtype = type(ttl)
+  if (vtype=='number') then
+    self.ttl = ttl
+  elseif(vtype=='boolean') then
+    self.persist = ttl
+  elseif(vtype=='nil') then
+    self.ttl = 300
+  end
+  if self.ttl then
+    self.ttl = s2ms(self.ttl)
+    self.expire = {}
+  end
+  self.store = {}
+
+  if self.ttl then
+    self.interval = interval and s2ms(interval) or s2ms(60)
+    assert(self.ttl > self.interval)
+  end
+end
+
+function Cache:onInterval()
+  local expired = 0
+  if self.expire then
+    local now = msnow()
+    for k,v in pairs(self.expire) do
+      if v < now then
+        expired = expired + 1
+        self:set(k)
+      end
+    end
+  end
+  self:emit('interval', self, expired)
+end
+
+function Cache:start()
+  if self._interval then
+    error('already start monitor')
+  end
+  if self.interval then
+    self._interval = timer.setInterval(self.interval, Cache.onInterval , self)
+  end
+end
+
+function Cache:stop()
+  if self._interval then
+    timer.clearInterval(self._interval)
+    self._interval = nil
+  end
+end
+
+function Cache:update_time(k, ttl)
+  local ttl = ttl and s2ms(ttl) or self.ttl
+  local now = msnow()
+  if self.expire[k] and self.expire[k] > ttl + now then
+    return
+  end
+  self.expire[k] = ttl + now
+end
+
+function Cache:get(k)
+  local val = self.store[k]
+  if val then
+    if self.persist == false then
+      self.store[k] = nil
+    elseif(self.ttl) then
+      self:update_time(k)
+    end
+  end
+  return val
+end
+
+function Cache:set(k, v, ttl)
+  self.store[k] = v
+  if self.ttl then
+    if v then
+      self:update_time(k, ttl)
+    else
+      self.expire[k] = nil
+    end
+  end
+end
+
+function Cache:clear()
+  self.store = {}
+  if self.expire then
+    self.expire = {}
+  end
+end
+
+function Cache:has(k)
+  return self.store[k] ~= nil
+end
+
+function Cache:length()
+  local c = 0
+  for _ in pairs(self.store) do
+    c = c + 1
+  end
+  return c
+end
+
+function Cache:keys()
+  local t = {}
+  for k, _ in pairs(self.store) do
+    table.insert(t, k)
+  end
+  return t
+end
+
+return Cache
+
---
+++ test-cache.lua
@@ -0,0 +1,57 @@
+local Cache = require'./cache'
+
+require('tap')(function (test)
+  test("Cache test with args", function()
+    local s = Cache:new()
+    assert(s:length()==0)
+    s:set('a','a')
+    assert(s:length()==1)
+    s:set('b','b')
+    assert(s:length()==2)
+    s:set('c','c')
+    assert(s:length()==3)
+    local keys = s:keys()
+    assert(#keys==3)
+    s:set('c')
+    assert(s:length()==2)
+    s:set('b')
+    assert(s:length()==1)
+    s:set('a')
+    assert(s:length()==0)
+    assert(#s.store==0 and #s.expire==0)
+    s:set('a','a')
+    assert(s:length()==1)
+  end)
+
+  test('Cache quick expire', function()
+    local timer = require'timer'
+    local uv = require'uv'
+    local s = Cache:new(4, 1)
+    s:start()
+    s:on('interval', function(self, expired)
+      print('expired', expired)
+      if(self:length()==0) then
+        s:stop()
+      end
+      assert(#(s:keys()) == s:length())
+      assert(#s.expire == #s.store)
+    end)
+
+    assert(s:length()==0)
+    s:set('a','a')
+    assert(s:length()==1)
+    s:set('b','b')
+    assert(s:length()==2)
+    s:set('c','c')
+    assert(s:length()==3)
+    local keys = s:keys()
+    assert(#keys==3)
+
+    timer.setTimeout(2000, function()
+      s:set('d','e')
+      assert(s:length()==4)
+    end)
+  end)
+
+end)
+
